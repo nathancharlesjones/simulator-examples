166ec52e3837bb3d06422fcc483f2629783445a3
Nathan Jones <nathancharlesjones@gmail.com>
2024-10-07 13:16:54 -0400

    Updated build.ninja and build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p build_shell/application/basic
-mkdir -p build_shell/application/advanced
-mkdir -p build_shell/hardware/x86
+mkdir -p build/application/basic
+mkdir -p build/application/advanced
+mkdir -p build/hardware/x86
 
 #######################################
 #                                     #
@@ -15,31 +15,31 @@ mkdir -p build_shell/hardware/x86
 ###################################################################################################################################################################################################
 
 # Compile basic/application.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0	-I application/basic -I hardware	-c application/basic/application.c 					-o build_shell/application/basic/application.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0	-I application/basic -I hardware	-c application/basic/application.c 					-o build/application/basic/application.o
 
 # Compile advanced/application.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c application/advanced/application.c 				-o build_shell/application/advanced/application.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c application/advanced/application.c 				-o build/application/advanced/application.o
 
 # Compile advanced/application_MainMajor.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c application/advanced/application_MainMajor.c		-o build_shell/application/advanced/application_MainMajor.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c application/advanced/application_MainMajor.c		-o build/application/advanced/application_MainMajor.o
 
 # Compile hardware/x86/simple-printf_basic.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/basic -I hardware 	-c hardware/x86/printf_basic.c 						-o build_shell/hardware/x86/simple-printf_basic.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/basic -I hardware 	-c hardware/x86/simple-printf_basic.c 				-o build/hardware/x86/simple-printf_basic.o
 
 # Compile hardware/x86/simple-printf_advanced.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/printf_advanced.c 					-o build_shell/hardware/x86/simple-printf_advanced.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/simple-printf_advanced.c 			-o build/hardware/x86/simple-printf_advanced.o
 
 # Compile hardware/x86/pretty-printf_basic.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/basic -I hardware 	-c hardware/x86/printf_basic.c 						-o build_shell/hardware/x86/pretty-printf_basic.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/basic -I hardware 	-c hardware/x86/pretty-printf_basic.c 				-o build/hardware/x86/pretty-printf_basic.o
 
 # Compile hardware/x86/pretty-printf_advanced_v1.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/printf_advanced_v1.c 				-o build_shell/hardware/x86/pretty-printf_advanced_v1.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/pretty-printf_advanced_v1.c 		-o build/hardware/x86/pretty-printf_advanced_v1.o
 
 # Compile hardware/x86/pretty-printf_advanced_v2.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/printf_advanced_v2.c 				-o build_shell/hardware/x86/pretty-printf_advanced_v2.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/pretty-printf_advanced_v2.c 		-o build/hardware/x86/pretty-printf_advanced_v2.o
 
 # Compile hardware/x86/serial_advanced.o
-gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/serial_advanced.c 					-o build_shell/hardware/x86/serial_advanced.o
+gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced -I hardware -c hardware/x86/serial_advanced.c 					-o build/hardware/x86/serial_advanced.o
 
 #######################################
 #                                     #
@@ -52,19 +52,19 @@ gcc 	-Wall -ffunction-sections -fdata-sections -g3 -O0 	-I application/advanced 
 ########################################################################################################################################################################################################################################################################################################################
 
 # Link simple-printf_basic.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/simple-printf_basic.map,--cref 				-I application/basic -I hardware 		build_shell/application/basic/application.o build_shell/hardware/x86/simple-printf_basic.o 					-lm 	-o build_shell/simple-printf_basic.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/simple-printf_basic.map,--cref 					-I application/basic -I hardware 		build/application/basic/application.o build/hardware/x86/simple-printf_basic.o 								-lm 	-o build/simple-printf_basic.elf
 
 # Link simple-printf_advanced.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/simple-printf_advanced.map,--cref 			-I application/advanced -I hardware 	build_shell/application/advanced/application.o build_shell/hardware/x86/simple-printf_advanced.o 			-lm 	-o build_shell/simple-printf_advanced.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/simple-printf_advanced.map,--cref 					-I application/advanced -I hardware 	build/application/advanced/application.o build/hardware/x86/simple-printf_advanced.o 						-lm 	-o build/simple-printf_advanced.elf
 
 # Link pretty-printf_basic.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/pretty-printf_basic.map,--cref 				-I application/basic -I hardware 		build_shell/application/basic/application.o build_shell/hardware/x86/pretty-printf_basic.o 					-lm 	-o build_shell/pretty-printf_basic.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/pretty-printf_basic.map,--cref 					-I application/basic -I hardware 		build/application/basic/application.o build/hardware/x86/pretty-printf_basic.o 								-lm 	-o build/pretty-printf_basic.elf
 
 # Link pretty-printf_advanced_v1.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/pretty-printf_advanced_v1.map,--cref 		-I application/advanced -I hardware 	build_shell/application/advanced/application.o build_shell/hardware/x86/pretty-printf_advanced_v1.o 		-lm 	-o build_shell/pretty-printf_advanced_v1.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/pretty-printf_advanced_v1.map,--cref 				-I application/advanced -I hardware 	build/application/advanced/application.o build/hardware/x86/pretty-printf_advanced_v1.o 					-lm 	-o build/pretty-printf_advanced_v1.elf
 
 # Link pretty-printf_advanced_v2.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/pretty-printf_advanced_v2.map,--cref 		-I application/advanced -I hardware 	build_shell/application/advanced/application.o build_shell/hardware/x86/pretty-printf_advanced_v2.o 		-lm 	-o build_shell/pretty-printf_advanced_v2.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/pretty-printf_advanced_v2.map,--cref 				-I application/advanced -I hardware 	build/application/advanced/application.o build/hardware/x86/pretty-printf_advanced_v2.o 					-lm 	-o build/pretty-printf_advanced_v2.elf
 
 # Link serial_advanced.out
-gcc 		-Wl,--gc-sections -Wl,-Map,build_shell/serial_advanced.map,--cref 					-I application/advanced -I hardware 	build_shell/application/advanced/application.o build_shell/hardware/x86/serial_advanced.o 					-lm 	-o build_shell/serial_advanced.elf
+gcc 		-Wl,--gc-sections -Wl,-Map,build/serial_advanced.map,--cref 						-I application/advanced -I hardware 	build/application/advanced/application.o build/hardware/x86/serial_advanced.o 								-lm 	-o build/serial_advanced.elf
